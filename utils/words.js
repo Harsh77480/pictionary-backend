09549685f518bd72e33f44e44dccb8b1dbc29390
Harsh <tech77480@gmail.com>
2025-10-04 07:17:32 +0000

    Extended words array

--- utils/words.js
+++ utils/words.js
@@ -1,7 +1,13 @@
 // utils/words.js
 module.exports = [
-  'apple', 'house', 'car', 'tree', 'dog',
-  'cat', 'boat', 'sun', 'moon', 'flower',
-  'keyboard', 'phone', 'pizza', 'guitar', 'bridge',
-  'bicycle', 'camera', 'hat', 'shoe', 'cake'
+  "Apple", "Ball", "Cat", "Dog", "House", "Tree", "Car", "Sun", "Moon", "Fish",
+  "Chair", "Bed", "Book", "Shoe", "Hat", "Cup", "Bird", "Flower", "Cloud", "Star",
+  "Ice cream", "Banana", "Table", "Phone", "Clock", "Key", "Door", "Pencil", "Toothbrush", "Pizza",
+  "Cake", "Camera", "Glasses", "Bike", "Hammer", "Spoon", "Fork", "Bottle", "Brush", "Soap",
+  "Balloon", "Ladder", "Tent", "Boat", "Plane", "Train", "Mountain", "River", "Island", "Bridge",
+  "Rainbow", "Snowman", "Fire", "Rain", "Lightning", "Spider", "Duck", "Turtle", "Lion", "Elephant",
+  "Giraffe", "Monkey", "Snake", "Robot", "Alien", "Ghost", "Vampire", "Princess", "Crown", "King",
+  "Queen", "Pirate", "Dragon", "Knight", "Witch", "Wizard", "Angel", "Devil", "Monster", "Dinosaur",
+  "Egg", "Cheese", "Hamburger", "Hotdog", "Sandwich", "Cookie", "Candy", "Tooth", "Hand", "Foot",
+  "Eye", "Nose", "Ear", "Heart", "Music", "Dance", "Jump", "Sleep", "Run", "Smile"
 ];
